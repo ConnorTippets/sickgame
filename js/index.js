6cb8610fdede219e3b904fd08f10eb0ed4b42b10
Con <50535379+ConnorTippets@users.noreply.github.com>
2024-02-17 20:31:33 -0600

    basic stuff

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var username;
+
+function startGame() {
+	username = document.getElementById("username").value;
+	document.getElementById("starter").remove();
+	alert(username + " awesomesauce!!!");
+};
