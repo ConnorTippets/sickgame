90f7388081e1aea505fd41d2658e6f0672330532
Con <50535379+ConnorTippets@users.noreply.github.com>
2024-02-18 16:31:32 -0600

    add i/o

--- js/game.js
+++ js/game.js
@@ -1,11 +1,27 @@
 var ctx, username;
 var start, prev;
 var x = 0;
+var keys = [];
+
+document.onkeydown = (event) => {
+	if (keys.indexOf(event.key) < 0 && document.pointerLockElement) {
+		keys.push(event.key);
+	};
+};
+
+document.onkeyup = (event) => {
+	keys.splice(keys.indexOf(event.key), 1);
+}
 
 function init(username, cont) {
 	ctx = cont;
 	username = username;
 	prev = window.performance.now();
+	
+	ctx.canvas.addEventListener("click", async () => {
+		await ctx.canvas.requestPointerLock();
+	});
+	
 	window.requestAnimationFrame(main);
 };
 
@@ -23,6 +39,7 @@ function loop(delta) {
 	fill("rgb(50 50 50)");
 	pixel(x, 1, "rgb(255 0 0)");
 	x = x + delta;
+	console.log(keys);
 };
 
 function main() {
