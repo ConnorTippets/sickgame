c583a5226bf5c0af3ba7a1934d8f36a896cbddb4
Con <50535379+ConnorTippets@users.noreply.github.com>
2024-02-18 22:44:19 -0600

    connect points

--- js/game.js
+++ js/game.js
@@ -46,13 +46,27 @@ function handle(delta) {
 	player.z -= flying;
 };
 
+function drawWall(x1, x2, b1, b2) {
+	dyb = b2-b1;
+	dx = x2- x1;
+	xs = x1;
+	if (dx == 0) { dx = 1; }
+	
+	for (x=x1; x < x2; x ++) {
+		y1 = dyb*(x-xs+0.5)/dx+b1
+		pixel(x, y1, "rgb(245 213 71)");
+	};
+};
+
 function draw() {
 	var cos = cosA[Math.floor(player.a)];
 	var sin = sinA[Math.floor(player.a)];
 	
 	//offset wall points by player
-	var x1 = 40 - player.x, y1 = 10 - player.y;
-	var x2 = 40 - player.x, y2 = 290 - player.y;
+	var x1 = 40 - player.x;
+	var y1 = 10 - player.y;
+	var x2 = 40 - player.x;
+	var y2 = 290 - player.y;
 	
 	//world X position
 	var wx = [];
@@ -77,8 +91,10 @@ function draw() {
 	sx.push(wx[1]*200/wy[1]+(width/2));
 	sy.push(wz[1]*200/wy[1]+(height/2));
 	
-	if (sx[0] > 0 && sx[0] < width && sy[0] > 0 && sy[0] < height) { pixel(sx[0], sy[0], "rgb(245 213 71)"); }
-	if (sx[1] > 0 && sx[1] < width && sy[1] > 0 && sy[1] < height) { pixel(sx[1], sy[1], "rgb(245 213 71)"); }
+	//if (sx[0] > 0 && sx[0] < width && sy[0] > 0 && sy[0] < height &&
+	//	sx[1] > 0 && sx[1] < width && sy[1] > 0 && sy[1] < height) {
+			drawWall(sx[0], sx[1], sy[0], sy[1]);
+	//}
 };
 
 function start() {
